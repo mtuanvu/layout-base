4719741ae801ce3e21083e12fb543f037d28f051
LeonCromwell <mailyhai814@gmail.com>
2023-10-12 23:52:35 +0700

    fix bug

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let username = id("username"),
   form = id("form"),
   errorMsg = classes("error"),
   successIcon = classes("success-icon"),
-  failureIon = classes("failure-icon");
+  failureIcon = classes("failure-icon");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -16,6 +16,54 @@ form.addEventListener("submit", (e) => {
   engine(username, 0, "Username cannot be blank");
   engine(email, 1, "Email cannot be blank");
   engine(password, 2, "Password cannot be blank");
+
+  // if (username.value === "") {
+  //   username.style.border = "2px solid red";
+  //   errorMsg[0].innerHTML = "Username cannot be blank";
+
+  //   //icon
+  //   failureIcon[0].style.opacity = "1";
+  //   successIcon[0].style.opacity = "0";
+  // } else {
+  //   username.style.border = "2px solid green";
+  //   errorMsg[0].innerHTML = "";
+
+  //   //icon
+  //   failureIcon[0].style.opacity = "0";
+  //   successIcon[0].style.opacity = "1";
+  // }
+
+  // if (email.value === "") {
+  //   email.style.border = "2px solid red";
+  //   errorMsg[1].innerHTML = "Email cannot be blank";
+
+  //   //icon
+  //   failureIcon[1].style.opacity = "1";
+  //   successIcon[1].style.opacity = "0";
+  // } else {
+  //   email.style.border = "2px solid green";
+  //   errorMsg[1].innerHTML = "";
+
+  //   //icon
+  //   failureIcon[1].style.opacity = "0";
+  //   successIcon[1].style.opacity = "1";
+  // }
+
+  // if (password.value === "") {
+  //   password.style.border = "2px solid red";
+  //   errorMsg[2].innerHTML = "Password cannot be blank";
+
+  //   //icon
+  //   failureIcon[2].style.opacity = "1";
+  //   successIcon[2].style.opacity = "0";
+  // } else {
+  //   password.style.border = "2px solid green";
+  //   errorMsg[2].innerHTML = "";
+
+  //   //icon
+  //   failureIcon[2].style.opacity = "0";
+  //   successIcon[2].style.opacity = "1";
+  // }
 });
 
 let engine = (id, serial, message) => {
